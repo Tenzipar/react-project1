8afc70b016d79617462930589c2c83f47e7469f1
Anjiru <zeroblack76@gmail.com>
2023-11-18 09:48:49 +0800

    first commit

---
+++ App.js
@@ -0,0 +1,22 @@
+
+
+import Navbar from './navbar';
+import Home from './Home';
+
+function App() {
+
+  // const title = "Hello world";
+  // const like = 50;
+  return (
+    <div className="App">
+        <Navbar />
+        <div className="content">
+          <Home />
+        </div>
+    </div>
+
+    
+  );
+}
+
+export default App;
